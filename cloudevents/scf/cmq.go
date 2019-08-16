a83aa3ae3879af1f488c0bd80b132a84a7233221
ritchiechen <ritchiechen@tencent.com>
2019-08-16 11:52:15 +0800

    fix cmq msg deserialize error

--- cloudevents/scf/cmq.go
+++ cloudevents/scf/cmq.go
@@ -1,5 +1,10 @@
 package scf
 
+import (
+	"encoding/json"
+	"strings"
+)
+
 // CMQEvent received as list
 type CMQEvent struct {
 	Records []CMQEventRecord `json:"Records"`
@@ -19,5 +24,22 @@ type CMQEventEntity struct {
 	MsgID            string   `json:"msgId"`
 	RequestID        string   `json:"requestId"`
 	MsgBody          string   `json:"msgBody"`
-	MsgTag           []string `json:"msgTag"`
+	MsgTag           []string `json:"-"`
+}
+
+// UnmarshalJSON implements the json.Unmarshaller interface
+func (entity *CMQEventEntity) UnmarshalJSON(data []byte) error {
+	type Alias CMQEventEntity
+	type Entity struct {
+		Alias
+		Tags string `json:"msgTag"`
+	}
+	var e Entity
+	err := json.Unmarshal(data, &e)
+	if err != nil {
+		return err
+	}
+	*entity = CMQEventEntity(e.Alias)
+	entity.MsgTag = strings.Split(e.Tags, ", ")
+	return nil
 }
---
+++ cloudevents/scf/cmq_test.go
@@ -0,0 +1,17 @@
+package scf_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/tencentyun/scf-go-lib/cloudevents/scf"
+)
+
+func TestCMQ(t *testing.T) {
+	data := `{"Records": [{"CMQ": {"msgBody": "aaaaaaaaaa", "msgId": "13510798882111490", "msgTag": "aaaa, bbbbb, cccc", "publishTime": "2019-08-16T10:48:49Z", "requestId": "2758374289357404466", "subscriptionName": "ritchiechen-cmq-test", "topicName": "ritchiechen-cmq-test", "topicOwner": 123456, "type": "topic"}}]}`
+	var event scf.CMQEvent
+	err := json.Unmarshal([]byte(data), &event)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
